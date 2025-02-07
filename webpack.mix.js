bb9470a9c2f92a430792cabd8f76b0a99f8c4b8f
Thrond Arne Throndsen <post@thrond.no>
2025-02-07 11:50:04 +0100

    Add support for Bootstrap 3 in modal components
    Introduce a new configuration option to enable Bootstrap 3 support with corresponding views, JavaScript, and styles. This enhances flexibility for projects using older versions of Bootstrap alongside the existing modal system. Updated the build process to include the new assets.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/modal.js', 'public/')
+    .js('resources/js/modal-bootstrap3.js', 'public/')
     .postCss("resources/css/modal.css", "public/", [
         require("tailwindcss"),
     ]);
